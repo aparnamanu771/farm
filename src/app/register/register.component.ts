3717f8112bbe25d25b90d90e2546d3ae750bae2e
manucrosspay <manu.kp@crosspaymt.com>
2023-03-25 15:22:16 +0530

    psw

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit{
   data="Get access to exclussive features by creating an Account"
-  
+  formdata={name:"",email:"",password:""};
+  submit=false;
   constructor(){}
 
   ngOnInit(): void {
      }
     onsubmit(){
+
+    }
+    onSubmit(){
+      console.log(this.formdata);
+      
 
     }
  
